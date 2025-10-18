1f8dcf4fafa4ca55eb45f3b202148b9cc18e5e73
farhanop <farhanop@uigm.ac.id>
2025-10-19 04:12:04 +0700

    Auto update

---
+++ git-auto-update.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d "C:\Users\farhanop\Documents\Projects\e-aset-app"
+git add .
+git commit -m "Auto update"
+git push origin main
+echo.
+echo ✅ Proyek e-aset-app berhasil diupdate ke GitHub.
+pause
